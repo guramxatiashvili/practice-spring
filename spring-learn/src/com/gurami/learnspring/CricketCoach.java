96a616fd26fa20fe313e3e6f13f6121b141067c9
Gurami khatiashvili <guramxatiashvili@gmail.com>
2021-01-26 22:16:08 +0100

    Injecting literal values

--- spring-learn/src/com/gurami/learnspring/CricketCoach.java
+++ spring-learn/src/com/gurami/learnspring/CricketCoach.java
@@ -4,12 +4,40 @@ public class CricketCoach implements Coach {
 
 	private FortuneService fortuneService;
 	
+	//add new fields for emalAddress and team
+	private String emailAddress;
+	private String team;
+	
+	
 	//Create a no-arg constructor
 	public CricketCoach() {
 		System.out.println("ChricketCoach: inside constructor testing");
 	}
 	
 	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+
+	public void setEmailAddress(String emailAddress) {
+		
+		System.out.println("ChricketCoach: inside the setter methor - setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+
+
+	public String getTeam() {
+		return team;
+	}
+
+
+	public void setTeam(String team) {
+		System.out.println("ChricketCoach: inside the setter methor - setTeam");
+		this.team = team;
+	}
+
+
 	public void setFortuneService(FortuneService fortuneService) {
 		System.out.println("ChricketCoach: inside the setter methor - setFortuneService");
 		this.fortuneService = fortuneService;
